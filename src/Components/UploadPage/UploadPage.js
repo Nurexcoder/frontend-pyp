1c5b3f03f6602514d17d38448f9cc2d8a3c87723
Vishal Vaibhav <vaibhavishal81@gmail.com>
2022-05-16 23:21:20 +0530

    Styled UploadPage With Styled Components AND Added a Box

--- src/Components/UploadPage/UploadPage.js
+++ src/Components/UploadPage/UploadPage.js
@@ -1,63 +1,69 @@
 import React from "react";
 
-import { TextField, Paper} from "@mui/material";
+import { TextField, Paper, Button, Box } from "@mui/material";
 import Autocomplete from "@mui/material/Autocomplete";
+import { styled } from "@mui/system";
 
-const style = {
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-  flexDirection: "column",
-  // width: "50%",
-  // height: "100%"
+const Style = styled(Box)`
+  display: flex;
+  justify-content: center;
+  margin: 38px;
+`;
 
-  // border: '2px solid black'
-};
+const PaperStyle = styled(Paper)`
+  width: 76rem;
+  height: 42rem;
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
 
-
-
+  h1 {
+    font-weight: bold;
+    font-size: 6.1vh;
+    margin-top: 32px;
+  }
+`;
 
 const Branch = [{ label: "CSE" }];
 const subjectCode = [{ label: "CS22101" }];
 const semester = [{ label: "1" }];
 const year = [{ label: "3000" }];
 
-
 function UploadPage() {
   return (
-    <>
-        <Paper elevation={10} sx={style}>
-      <form >
+    <Style>
+      <PaperStyle elevation={10}>
+        <form>
           <h1>The Art of Sharing</h1>
           <TextField placeholder="Name" />
           <Autocomplete
-            placeholder="Subject Code"
-            disablePortal
             options={subjectCode}
             sx={{ width: 300 }}
-            renderInput={(params) => <TextField {...params} />}
+            renderInput={(params) => <TextField placeholder="Subject Code" {...params} />}
           />
           <Autocomplete
-            disablePortal
             options={semester}
             sx={{ width: 300 }}
-            renderInput={(params) => <TextField {...params} />}
+            renderInput={(params) => <TextField placeholder="Semester" {...params} />}
           />
           <Autocomplete
-            disablePortal
             options={year}
             sx={{ width: 300 }}
-            renderInput={(params) => <TextField {...params} />}
+            renderInput={(params) => <TextField placeholder="Year" {...params} />}
           />
           <Autocomplete
             disablePortal
             options={Branch}
             sx={{ width: 300 }}
-            renderInput={(params) => <TextField {...params} />}
+            renderInput={(params) => <TextField placeholder="Branch" {...params} />}
           />
-      </form>
-        </Paper>
-    </>
+
+          <Button variant="contained">Done</Button>
+        </form>
+      </PaperStyle>
+    </Style>
   );
 }
 
