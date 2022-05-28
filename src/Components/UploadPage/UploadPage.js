1d50cc31f34f42fbbc956a545af569a60b259d1f
Vishal Vaibhav <vaibhavishal81@gmail.com>
2022-05-28 13:16:59 +0530

    Upload Page Styling Completed

--- src/Components/UploadPage/UploadPage.js
+++ src/Components/UploadPage/UploadPage.js
@@ -1,8 +1,9 @@
 import React from "react";
 
-import { TextField, Paper, Button, Box } from "@mui/material";
+import { TextField, Paper, Button, Box, Input, Grid } from "@mui/material";
 import Autocomplete from "@mui/material/Autocomplete";
 import { styled } from "@mui/system";
+import { shadows } from "@mui/system";
 
 const Style = styled(Box)`
   display: flex;
@@ -11,8 +12,9 @@ const Style = styled(Box)`
 `;
 
 const PaperStyle = styled(Paper)`
-  width: 76rem;
-  height: 42rem;
+  width: 34rem;
+  height: 38rem;
+  border-radius: 19px;
   form {
     display: flex;
     flex-direction: column;
@@ -21,10 +23,106 @@ const PaperStyle = styled(Paper)`
 
   h1 {
     font-weight: bold;
-    font-size: 6.1vh;
-    margin-top: 32px;
+    /* font-size: 6.1vh; */
+    margin-top: 62px;
+    margin-bottom: 32px;
   }
 `;
+
+const StyleName = {
+
+  height: '42px',
+  // border: '2px solid black',
+  borderRadius: '5px',
+  width: '22.6rem',
+  paddingLeft: "15px",
+  paddingTop: '8px',
+
+  boxShadow: '0.5px 2px 9px #524f504d',
+  // boxShadow: '0px 6px 6px -3px rgb(0 0 0 / 20%), 0px 10px 14px 1px rgb(0 0 0 / 14%), 0px 4px 18px 3px rgb(0 0 0 / 12%)',
+
+  "& input::placeholder": {
+    fontSize: "20px",
+    fontWeight: '800',
+  },
+};
+
+const BoxStyle = styled(Box)`
+  display: flex;
+`;
+
+// Styling With Classes
+const AutoStyle = {
+  "& .MuiAutocomplete-inputRoot": {
+    // border: "2px solid black",
+    width: "24rem",
+    height: '52px',
+    borderRadius: "6px",
+    paddingLeft: "15px",
+    marginTop: "24px",
+    marginBottom: "12px",
+
+    boxShadow: '0.5px 2px 9px #524f504d',
+  },
+
+  "& input::placeholder": {
+    fontSize: "20px",
+    fontWeight: 'bold',
+  },
+
+};
+
+const AutoFirstText = {
+
+  "& .MuiAutocomplete-inputRoot": {
+    // border: "2px solid black",
+    width: "11.7rem",
+    height: '52px',
+    borderRadius: "5px",
+    paddingLeft: "15px",
+
+    boxShadow: '0.5px 2px 9px #524f504d',
+  },
+
+  "& input::placeholder": {
+    fontSize: "20px",
+    fontWeight: 'bold',
+  }
+};
+
+const AutoSecondText = {
+  "& .MuiAutocomplete-inputRoot": {
+    // border: "2px solid black",
+    width: "11.5rem",
+    height: '52px',
+    borderRadius: "5px",
+    paddingLeft: "15px",
+    marginLeft: "10px",
+
+    boxShadow: '0.5px 2px 9px #524f504d',
+  },
+
+  "& input::placeholder": {
+    fontSize: "20px",
+    fontWeight: 'bold',
+  }
+};
+
+const ButtonStyle = {
+  marginTop: '3rem',
+  marginLeft: '19rem',
+  minWidth: '86px',
+  borderRadius: '12px',
+  textTransform: 'none',
+  fontSize: '17px',
+  lineHeight: '1.5',
+  backgroundColor: '#2b92f8',
+
+  "& 	.MuiButton-text": {
+    backgroundColor: "black",
+    border: "2px solid red",
+  },
+};
 
 const Branch = [{ label: "CSE" }];
 const subjectCode = [{ label: "CS22101" }];
@@ -37,30 +135,81 @@ function UploadPage() {
       <PaperStyle elevation={10}>
         <form>
           <h1>The Art of Sharing</h1>
-          <TextField placeholder="Name" />
-          <Autocomplete
-            options={subjectCode}
-            sx={{ width: 300 }}
-            renderInput={(params) => <TextField placeholder="Subject Code" {...params} />}
+          <TextField
+            id="standard-basic"
+            variant="standard"
+            sx={StyleName}
+            placeholder="Name"
+            InputProps={{disableUnderline: true}}
+            elevation={5}
           />
-          <Autocomplete
-            options={semester}
-            sx={{ width: 300 }}
-            renderInput={(params) => <TextField placeholder="Semester" {...params} />}
-          />
-          <Autocomplete
-            options={year}
-            sx={{ width: 300 }}
-            renderInput={(params) => <TextField placeholder="Year" {...params} />}
-          />
-          <Autocomplete
-            disablePortal
-            options={Branch}
-            sx={{ width: 300 }}
-            renderInput={(params) => <TextField placeholder="Branch" {...params} />}
-          />
+          <Box>
+            <Autocomplete
+              options={subjectCode}
+              // sx={{ width: 300 }}
+              sx={AutoStyle}
+              renderInput={(params) => (
+                <TextField
+                  placeholder="Subject Code"
+                  variant="standard"
+                  {...params}
+                  InputProps={{ ...params.InputProps, disableUnderline: true }}
+                />
+              )}
+            />
+          </Box>
 
-          <Button variant="contained">Done</Button>
+          <BoxStyle>
+            <Autocomplete
+              options={semester}
+              // sx={{ width: 300 }}
+              sx={AutoFirstText}
+              renderInput={(params) => (
+                <TextField
+                  placeholder="Semester"
+                  variant="standard"
+                  {...params}
+                  InputProps={{ ...params.InputProps, disableUnderline: true }}
+                />
+              )}
+            />
+            <Autocomplete
+              options={year}
+              // sx={{ width: 300 }}
+              sx={AutoSecondText}
+              renderInput={(params) => (
+                <TextField
+                  placeholder="Year"
+                  variant="standard"
+                  {...params}
+                  InputProps={{ ...params.InputProps, disableUnderline: true }}
+                />
+              )}
+            />
+          </BoxStyle>
+
+          
+            <Autocomplete
+              disablePortal
+              options={Branch}
+              // sx={{ width: 300 }}
+              sx={AutoStyle}
+              renderInput={(params) => (
+                <TextField
+                  placeholder="Branch"
+                  variant="standard"
+                  {...params}
+                  InputProps={{ ...params.InputProps, disableUnderline: true }}
+                />
+              )}
+            />
+          
+
+          {/* <Grid> */}
+            <Button variant="contained" sx={ButtonStyle}>
+              Done
+            </Button>
+          {/* </Grid> */}
         </form>
       </PaperStyle>
     </Style>
